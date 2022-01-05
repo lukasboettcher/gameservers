cfa68eb5b0ec7591b0b605e50be630c7d4f8f13c
lukasboettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-01-05 03:10:21 +0100

    change gmod gamemode to terrortown

--- gmod/start-gmod.sh
+++ gmod/start-gmod.sh
@@ -6,5 +6,5 @@ steamcmd/steamcmd.sh +login anonymous +force_install_dir /data/tf2_ds +app_updat
 sed -i -e 's#\./steam\.sh #\./steamcmd\.sh #g' gmod_ds/srcds_run # edit autoupdate for server version
 gmod_ds/srcds_run -game garrysmod -console -usercon  -port 27015 -net_port_try 1 -maxplayers 24 \
         -autoupdate -steam_dir /data/steamcmd -steamcmd_script /data/steamcmd/runscript_gmod \
-        +sv_pure 1 +map ttt_minecraft_b5 +host_workshop_collection 286987864 -authkey "${API}" \
+        +gamemode terrortown +map ttt_minecraft_b5 +host_workshop_collection 286987864 -authkey "${API}" \
         +rcon_password "${PASS_RCON}" +sv_password "${PASS}" +sv_setsteamaccount "${GSLT}"
