2c33188fc4cbeb5963aa39bb3dee3332b65999b5
lukasboettcher <1340215+lukasboettcher@users.noreply.github.com>
2023-01-04 05:40:02 +0100

    Update start-arma.sh

--- arma/start-arma.sh
+++ arma/start-arma.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-steamcmd/steamcmd.sh +login lukasboettcher +force_install_dir /data/arma_ds +app_update 233780 +quit
+steamcmd/steamcmd.sh +login steam_username +force_install_dir /data/arma_ds +app_update 233780 +quit
 cd arma_ds;./arma3server -name=Arma3_Server -config=server.cfg
