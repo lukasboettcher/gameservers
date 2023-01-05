35340e5ed5d2c06f5ff7a6c56a2795c5ddafda25
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2023-01-05 02:35:39 +0100

    improve mc start script, only copy settings on first launch

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -1,6 +1,9 @@
-# copy server / settings file
-cp /files/server.properties /data/server.properties
-cp /files/server.jar /data/server.jar
+#!/bin/sh
+# copy server / settings files
+[ ! -e /data/server.properties ] && cp /srv/server.properties /data/server.properties
+cp /srv/server.jar /data/server.jar
+
+echo "eula=true" > eula.txt
 
 # finally start the minecraft server
 java -jar -Xms4G -Xmx4G server.jar
