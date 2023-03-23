64fb7190664dadc1d772d30bae6428b61f5650b7
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2023-03-24 00:36:41 +0100

    rename start script and add persistent dirs

---
+++ gmod/start-server.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+/data/persistent/steamcmd/steamcmd.sh +force_install_dir /data/persistent/gmod_ds +login anonymous +app_update 4020 +quit
+/data/persistent/steamcmd/steamcmd.sh +force_install_dir /data/persistent/css_ds +login anonymous +app_update 232330 +quit
+/data/persistent/steamcmd/steamcmd.sh +force_install_dir /data/persistent/tf2_ds +login anonymous +app_update 232250 +quit
+
+cp $HOME/cfgs/*.cfg /data/persistent/gmod_ds/garrysmod/cfg/
+sed -i -e 's#\./steam\.sh #\./steamcmd\.sh #g' /data/persistent/gmod_ds/srcds_run # edit autoupdate for server version
+/data/persistent/gmod_ds/srcds_run -game garrysmod -console -usercon  -port 27015 -net_port_try 1 -maxplayers 24 \
+        -autoupdate -steam_dir /data/persistent/steamcmd -steamcmd_script $HOME/cfgs/steamcmd_runscript \
+        +gamemode terrortown +map ttt_minecraft_b5 +host_workshop_collection 286987864 -authkey "${API}" \
+        +rcon_password "${PASS_RCON}" +sv_password "${PASS}" +sv_setsteamaccount "${GSLT}"
