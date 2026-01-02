000f7d548de723c5e8d9155b09c743aef9b08355
lukasboettcher <1340215+lukasboettcher@users.noreply.github.com>
2026-01-02 04:11:45 +0100

    add me as admin

--- gmod/start-server.sh
+++ gmod/start-server.sh
@@ -4,6 +4,7 @@
 /data/persistent/steamcmd/steamcmd.sh +force_install_dir /data/persistent/tf2_ds +login anonymous +app_update 232250 +quit
 
 cp $HOME/cfgs/*.cfg /data/persistent/gmod_ds/garrysmod/cfg/
+cp $HOME/cfgs/users.txt /data/persistent/gmod_ds/garrysmod/settings/users.txt
 sed -i -e 's#\./steam\.sh #\./steamcmd\.sh #g' /data/persistent/gmod_ds/srcds_run # edit autoupdate for server version
 /data/persistent/gmod_ds/srcds_run -game garrysmod -console -usercon  -port 27015 -net_port_try 1 -maxplayers 24 \
         -autoupdate -steam_dir /data/persistent/steamcmd -steamcmd_script $HOME/cfgs/steamcmd_runscript \
